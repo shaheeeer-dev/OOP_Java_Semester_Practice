7e2bf74188f1d5b53e7c3adb30822448e63dd87a
Muhammad Shaheer <shaheeramazon1@gmail.com>
2026-01-13 16:56:01 +0500

    Add Student class practice problem

--- src/Basics/Classes_Objects/Student.java
+++ src/Basics/Classes_Objects/Student.java
@@ -2,35 +2,41 @@
 
 package Basics.Classes_Objects;
 
-public class Student {
-    int id; //Instance variables
+public class Student {  //Student class
+    //Instance variables
+    int id;
     String name;
     double marks;
 
-    public void display(){ //display Method
+    //display Method
+    public void display(){
         System.out.println("ID: "+id+" Name: "+ name + " Marks: "+marks);
     }
     public static void main(String[] args) {
-        Student s1 = new Student(); //object creation
+        //object creation
+        Student s1 = new Student();
         Student s2 = new Student();
         Student s3 = new Student();
 
+        //initializing objects
         s1.id = 101;
-        s2.id = 102;
-        s3.id = 103;
-
         s1.name = "Shaheer";
-        s2.name = "Ali";
-        s3.name = "Sarah";
-
         s1.marks = 85.7;
+
+        s2.id = 102;
+        s2.name = "Ali";
         s2.marks = 79.5;
+
+        s3.id = 103;
+        s3.name = "Sarah";
         s3.marks = 85.2;
 
+        //display function call
         s1.display();
         s2.display();
         s3.display();
 
+        //Average marks
         System.out.println(
                 "Average marks: "+(s1.marks+s2.marks+ s3.marks)/3
         );

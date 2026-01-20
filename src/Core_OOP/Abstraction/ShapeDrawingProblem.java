503bf10554f68673f35798f79848388515e2a943
Muhammad Shaheer <shaheeramazon1@gmail.com>
2026-01-20 23:18:45 +0500

    added Shape Drawing class Problem

---
+++ src/Core_OOP/Abstraction/ShapeDrawingProblem.java
@@ -0,0 +1,31 @@
+//Problem 5: Shape Drawing System
+
+package Core_OOP.Abstraction;
+
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    @Override
+    void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+public class ShapeDrawingProblem {
+    public static void main(String[] args) {
+        Shape s1 = new Circle();
+        Shape s2 = new Rectangle();
+
+        s1.draw();
+        s2.draw();
+    }
+}
